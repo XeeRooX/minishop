3f5ea8ba9a786f37bdf0fea4ec9a2c1d4b44838d
XeeRooX <akkaunt7931@gmail.com>
2023-04-03 19:04:19 +0500

    Set relationalship in CartItem, Product, ProductType

--- minishop/Models/CartItem.cs
+++ minishop/Models/CartItem.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public int Count { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public int CartId { get; set; }
+        public Cart Cart { get; set; } = null!;
     }
 }
--- minishop/Models/Product.cs
+++ minishop/Models/Product.cs
@@ -6,5 +6,9 @@
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public double Price { get; set; }
+
+        public List<CartItem> CartItem { get; set; } = new();
+        public int TypeProductId { get; set; }
+        public TypeProduct TypeProduct { get; set; } = null!;
     }
 }
