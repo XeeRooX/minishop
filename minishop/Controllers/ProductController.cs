5b17ad3b80dc8e52e747d5da60a03fc75bd0fc86
CvCvCvv <decomen123@gmail.com>
2023-04-06 00:39:32 +0500

    edit

--- minishop/Controllers/ProductController.cs
+++ minishop/Controllers/ProductController.cs
@@ -8,5 +8,15 @@ namespace minishop.Controllers
         {
             return View();
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
     }
 }
