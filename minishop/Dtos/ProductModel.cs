92b7880e7577e545b3617783b3052be655125be8
CvCvCvv <decomen123@gmail.com>
2023-04-07 18:46:29 +0500

    merge models and controllers branch

---
+++ minishop/Dtos/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using minishop.Models;
+
+namespace minishop.Dtos
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public double Price { get; set; }
+        public int TypeProductId { get; set; }
+
+        IFormFile Foto { get; set; } = null!;
+    }
+}
